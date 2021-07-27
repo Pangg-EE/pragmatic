13a3c183ad766ff268d7aa610bba189002c4f093
Pangg-EE <kwangho.kim@vntgcorp.com>
2021-07-27 21:15:10 +0900

    djnagho courser11-1 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world .html')
+    return render(request, 'accountapp/hello_world.html')

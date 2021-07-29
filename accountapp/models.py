82bb755bf8f2299a0ce8c2f9e1f340f6ce7524d1
Pangg-EE <kwangho.kim@vntgcorp.com>
2021-07-29 20:24:03 +0900

    django course 20 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
 class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null=False) # 해당 내용은 null 처리할 수 없다 !
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            # render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})

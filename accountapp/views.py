495e7f3ad59f8ff38c2205ccac50ccfa44664b14
Pangg-EE <kwangho.kim@vntgcorp.com>
2021-08-11 01:25:41 +0900

    view.py 내부 hello_world reverse_lazy 수정

--- accountapp/views.py
+++ accountapp/views.py
@@ -19,7 +19,7 @@ from articleapp.models import Article
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/create.html'
 
 
@@ -43,7 +43,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership, 'get')
